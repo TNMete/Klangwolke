d0877d10d5a40a99d0538d51a69c06ca908fc726
Macky232323 <a.manhardt@mackalex.de>
2025-04-02 12:16:58 +0200

    song-Pfade geändert

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const playlist = [
-        { title: 'No Rival', artist: 'Alaina Cross', genre: 'Pop', time: '3:18', src: 'http://127.0.0.1:5500/songs/Alaina Cross, Maestro Chives, Egzod - No Rival [NCS Release].mp3' },
-        { title: 'Move Like This', artist: 'Bad Computer', genre: 'Pop', time: '3:35', src: 'http://127.0.0.1:5500/songs/Bad Computer - Move Like This [NCS Release].mp3' },
+        { title: 'No Rival', artist: 'Alaina Cross', genre: 'Pop', time: '3:18', src: '../songs/Alaina Cross, Maestro Chives, Egzod - No Rival [NCS Release].mp3' },
+        { title: 'Move Like This', artist: 'Bad Computer', genre: 'Pop', time: '3:35', src: '../songs/Bad Computer - Move Like This [NCS Release].mp3' },
     ];
 
     const tableBody = document.querySelector('#playlistTable tbody');
