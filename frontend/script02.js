1523acb4301de75622fa413564838acb3b096da6
Macky232323 <a.manhardt@mackalex.de>
2025-04-02 12:43:19 +0200

    Scriptdatei Name geändert

---
+++ frontend/script02.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const playlist = [
+        { title: 'No Rival', artist: 'Alaina Cross', genre: 'Pop', time: '3:18', src: '../songs/Alaina Cross, Maestro Chives, Egzod - No Rival [NCS Release].mp3' },
+        { title: 'Move Like This', artist: 'Bad Computer', genre: 'Pop', time: '3:35', src: '../songs/Bad Computer - Move Like This [NCS Release].mp3' },
+    ];
+
+    const tableBody = document.querySelector('#playlistTable tbody');
+    const audioPlayer = document.querySelector('#audioPlayer');
+
+    playlist.forEach(song => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${song.title}</td>
+            <td>${song.artist}</td>
+            <td>${song.genre}</td>
+            <td>${song.time}</td>
+        `;
+        row.addEventListener('click', () => {
+            audioPlayer.src = song.src;
+            audioPlayer.play();
+        });
+        tableBody.appendChild(row);
+    });
+});
